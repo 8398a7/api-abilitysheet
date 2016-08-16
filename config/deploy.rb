bbd85e34a909187c1e85fc3bd6e7f719a449646d
839 <8398a7@gmail.com>
2016-08-16 11:29:54 +0900

    refs #7 add deploy tasks and staging setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,37 @@
-# config valid only for current version of Capistrano
 lock '3.6.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'api-abilitysheet'
+set :repo_url, 'https://github.com/8398a7/api-abilitysheet.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :deploy_to, '/var/www/app/api-abilitysheet'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :pty, true
+append :linked_files, '.env'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :default_env, path: '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
+set :rbenv_type, :system
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+namespace :deploy do
+  desc 'build abilitysheet'
+  task :build do
+    on roles(:app) do
+      execute "cd #{release_path}; rm .env; cp ../../shared/.env ."
+      execute "cd #{release_path}; docker build -t abilitysheet ."
+    end
+  end
+  task :remove do
+    on roles(:app) do
+      res = capture 'docker ps'
+      execute 'docker stop api-abilitysheet; docker rm api-abilitysheet' if res.include?('api-abilitysheet')
+    end
+  end
+  task :run do
+    on roles(:app) do
+      execute 'docker run -d -p 8080:8080 --name=api-abilitysheet abilitysheet'
+    end
+  end
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+  after 'symlink:linked_files', :build
+  after :build, :remove
+  after :remove, :run
+end
